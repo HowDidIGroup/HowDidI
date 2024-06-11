f94b3da8b069f28caa6b48982d9a2111bc8470b7
T-Mao <m737579372@gmail.com>
2024-06-10 23:59:53 -0700

    Initial Commit

---
+++ HowDidI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HowDidI
+//
+//  Created by Tongze Mao on 6/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HowDidI/HowDidIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HowDidIApp.swift
+//  HowDidI
+//
+//  Created by Tongze Mao on 6/10/24.
+//
+
+import SwiftUI
+
+@main
+struct HowDidIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
