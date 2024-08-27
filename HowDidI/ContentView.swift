c775cd1d9ba2e982e3398b2bf483e3c3af42fd4b
T-Mao <m737579372@gmail.com>
2024-08-27 00:20:47 -0700

    You Might Like

--- HowDidI/ContentView.swift
+++ HowDidI/ContentView.swift
@@ -61,6 +61,22 @@ struct HomeView: View {
                         .cornerRadius(8)
                         .padding(.horizontal)
                         
+                        // You Might Like Section
+                            VStack(alignment: .leading, spacing: 10) {
+                                                    Text("You Might Like >")
+                                                        .font(.headline)
+                                                        .padding(.horizontal)
+                                                    
+                                                    ScrollView(.horizontal, showsIndicators: false) {
+                                                        HStack(spacing: 20) {
+                                                            ForEach(recommendedSchools, id: \.self) { school in
+                                                                SchoolRecommendationView(school: school)
+                                                            }
+                                                        }
+                                                        .padding(.horizontal)
+                                                    }
+                                                }
+                        
                         // List of applications
                         VStack(alignment: .leading) {
                             ApplicationView(name: "yale", school: "To Yale", logo: "yale_logo")
@@ -300,5 +316,45 @@ struct AboutView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct SchoolRecommendationView: View {
+    var school: School
+
+    var body: some View {
+        NavigationLink(destination: SchoolDetailView(schoolName: school.name)) {
+            VStack {
+                Image(school.logo)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 60, height: 60)
+                Text(school.name)
+                    .font(.caption)
+            }
+        }
+    }
+}
+
+
+struct School: Hashable {
+    var name: String
+    var logo: String
+}
+
+let recommendedSchools = [
+    School(name: "Yale", logo: "YaleLogo"),
+    School(name: "Harvard", logo: "HarvardLogo"),
+    School(name: "UCB", logo: "UCBLogo"),
+    School(name: "UCSD", logo: "UCSDLogo")
+]
+
+
+struct SchoolDetailView: View {
+    var schoolName: String
+
+    var body: some View {
+        Text("Welcome to \(schoolName)!")
+            .navigationBarTitle(Text(schoolName), displayMode: .inline)
     }
 }
