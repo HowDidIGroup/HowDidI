15c4598b625b270f22f9c827975e646ae338d337
Emily Gao Wang <egwang1@uci.edu>
2024-08-29 12:46:38 -0700

    modify resume page

--- HowDidI/ApplicationView.swift
+++ HowDidI/ApplicationView.swift
@@ -53,10 +53,10 @@ struct ApplicationDetailView: View {
     let logo: String
     
     var body: some View {
-        VStack(alignment: .leading) {
+        VStack(alignment: .leading, spacing: 0) {
             // Title and logo at the top
-            HStack(alignment: .center) {
-                VStack() {
+            HStack(alignment: .top) {
+                VStack(alignment: .leading) {
                     Text(school)
                         .font(.title)
                         .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
@@ -65,6 +65,15 @@ struct ApplicationDetailView: View {
                         .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
                         .foregroundColor(.gray)
                 }
+                Text("Resume/CV")
+                    .font(.subheadline)
+                    .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                    .foregroundColor(.white)
+                    .padding(.horizontal, 10)
+                    .padding(.vertical, 7)
+                    .background(Color(hex: "1CAD9F"))
+                    .cornerRadius(18)
+                    .padding(.leading, 8)
                 Spacer()
                 Image(logo)
                     .resizable()
@@ -72,27 +81,55 @@ struct ApplicationDetailView: View {
                     .frame(height: 60)
             }
             .padding(.horizontal, 25)
+            .padding(.bottom, 0)
 
             // PDF Viewer
-            GeometryReader { geometry in
-                VStack(spacing: 0) {
-                    TabView {
-                        PDFViewer(pdfName: "yale_resume_1")
-                            .frame(width: geometry.size.width, height: geometry.size.width * 1.3)
-                        
-                        PDFViewer(pdfName: "yale_resume_2")
-                            .frame(width: geometry.size.width, height: geometry.size.width * 1.3)
-                        
-                        PDFViewer(pdfName: "yale_resume_3")
-                            .frame(width: geometry.size.width, height: geometry.size.width * 1.3)
-                    }
-                    .tabViewStyle(PageTabViewStyle(indexDisplayMode: .always))
+            VStack(spacing: 0) {
+                TabView {
+                    PDFViewer(pdfName: "yale_resume_1")
+                        .frame(width: UIScreen.main.bounds.width * 0.95, height: UIScreen.main.bounds.width * 0.95 * 1.3)
+                    
+                    PDFViewer(pdfName: "yale_resume_2")
+                        .frame(width: UIScreen.main.bounds.width * 0.95, height: UIScreen.main.bounds.width * 0.95 * 1.3)
+                    
+                    PDFViewer(pdfName: "yale_resume_3")
+                        .frame(width: UIScreen.main.bounds.width * 0.95, height: UIScreen.main.bounds.width * 0.95 * 1.3)
                 }
-                .frame(width: geometry.size.width, height: geometry.size.width * 1.3)
-                .alignmentGuide(.top) { _ in 0 } // Aligns the content to the top
+                .tabViewStyle(PageTabViewStyle(indexDisplayMode: .always))
+                .padding(.top, 0)
+                .padding(.bottom, 0)
             }
+            .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.width * 1.3)
+            .padding(.top, 0)
+            .padding(.bottom, 0)
+            
+            // Comment
+            VStack(alignment: .leading) {
+                // Comment Title
+                Text("Comment")
+                    .font(.subheadline)
+                    .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                    .foregroundColor(.white)
+                    .padding(.horizontal, 10)
+                    .padding(.vertical, 7)
+                    .background(Color(hex: "1CAD9F"))
+                    .cornerRadius(18)
+                    .padding(.leading, 8)
+                    .padding(.top, 0)
+                // Comment Text
+                Text("Be specific! And never go over stuff casually! Be specific! And never go over stuff casually! Be specific! And never go over stuff casually!")
+                    .font(.subheadline)
+                    .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                    .padding(.horizontal, 10)
+                    .padding(.leading, 8)
+            }
+            .padding(.top, 0)
+            
+            Spacer()
+
         }
-        .padding(.top, 10)
+        .padding(.top, 5)
+        .navigationBarBackButtonHidden(true) // Hides the back button
     }
 }
 
